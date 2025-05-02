824f4ddb53245e2098c11462a3d0aa7058051367
ColdenJohnson <cbj19@duke.edu>
2025-05-02 14:55:27 -0400

    TODO: add note

--- components/ImgDisplay.jsx
+++ components/ImgDisplay.jsx
@@ -3,10 +3,10 @@ import { useFirebaseImage } from '@/hooks/useFirebaseImage';
 import { ActivityIndicator, View } from 'react-native';
 
 export function ImgFromUrl({ url, style, resizeMode = "contain" }) {
+  // TODO: this is likely where we're not calling firebase (just getting from a general URL), and thus not displaying image until reload
   if (!url) {
     return null;
   }
-
   return (
     <Image
       source={{ uri: url }}
@@ -19,6 +19,7 @@ export function ImgFromUrl({ url, style, resizeMode = "contain" }) {
 // Proper Component
 export function ImgDisplay({ filename, style, resizeMode = "contain" }) {
   const imageUrl = useFirebaseImage(filename);
+  console.log('imageUrl', imageUrl)
   if (!imageUrl) {
     return (
       <View style={[{ width: 200, height: 200, justifyContent: 'center', alignItems: 'center' }, style]}>
